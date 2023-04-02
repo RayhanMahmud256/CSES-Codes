e05bd9a75aa3ff4c650faf3d6c3e37b360717c4c
Rayhan Mahmud <99743908+RayhanMahmud256@users.noreply.github.com>
2023-04-02 19:07:20 +0600

    Add files via upload

---
+++ Increasing Array(4).cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,greater<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define ull unsigned long long int
+
+int main()
+{
+    optimize();
+    ll n;
+    cin>>n;
+    while(1){
+        cout<<"M"<<endl;
+    }
+    return 0;
+}
